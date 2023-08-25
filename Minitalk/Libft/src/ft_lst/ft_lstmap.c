a6b1e5052b86248c4a1c7e78be0fec2589e14769
Thuy Quematon <115153368+Thuggonaut@users.noreply.github.com>
2023-08-25 16:36:42 +0930

    Update ft_lstmap.c

--- Minitalk/Libft/src/ft_lst/ft_lstmap.c
+++ Minitalk/Libft/src/ft_lst/ft_lstmap.c
@@ -3,31 +3,6 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
-	t_list	*current_node;
-
-	if (!lst)
-		return (NULL);
-	new_list = NULL;
-	while (lst)
-	{
-		current_node = ft_lstnew(f(lst->content));
-		if (!current_node)
-		{
-			ft_lstclear(&new_list, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&new_list, current_node);
-		lst = lst->next;
-	}
-	return (new_list);
-}
-
-/*
-TEST FRANCINETTE STRICT:
-
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list	*new_list;
 	t_list	*head;
 	void	*content;
 
