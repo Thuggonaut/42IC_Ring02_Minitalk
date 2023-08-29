b96af2e024c1d5e392fadc244beb901d2aca765c
Thuy Quematon <115153368+Thuggonaut@users.noreply.github.com>
2023-08-29 20:05:48 +0930

    Update libft.h

--- Minitalk/Libft/inc/libft.h
+++ Minitalk/Libft/inc/libft.h
@@ -46,7 +46,7 @@ char	*ft_strdup(const char *s);
 char    *ft_strjoin(char const *s1, char const *s2);
 size_t  ft_strlcat(char *dst, const char *src, size_t buffsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t  ft_strlen(char *s);
+size_t  ft_strlen(const char *s);
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
 char    *ft_strnstr(const char *big, const char *little, size_t n);
