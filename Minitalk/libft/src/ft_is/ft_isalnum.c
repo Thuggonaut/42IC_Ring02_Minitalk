8bd0cd18bad6f01c8d9956f1383bc78f0f846641
Thuy Quematon <thuytkq@outlook.com>
2023-09-03 13:54:33 +0930

    u

--- Minitalk/libft/src/ft_is/ft_isalnum.c
+++ Minitalk/libft/src/ft_is/ft_isalnum.c
@@ -2,9 +2,9 @@
 
 int ft_isalnum(int c)
 {
-    if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || (c >= '0' && c <= '9'))
-    {
-        return (1);
-    }
+	if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || (c >= '0' && c <= '9'))
+	{
+		return (1);
+	}
 	return (0);
 }
